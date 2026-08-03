9326ccf85160095f9758552058c79ec0b16c409f
Omkar-ijardar <omkarijardar@gmail.com>
2026-08-04 02:37:09 +0530

    implement Feign clients for microservice communications

---
+++ backend/Bank-Admin/src/main/java/com/bank/admin/client/TransactionServiceClient.java
@@ -0,0 +1,27 @@
+package com.bank.admin.client;
+
+import java.math.BigDecimal;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bank.admin.config.FeignConfig;
+
+@FeignClient(
+        name = "TRANSACTION-SERVICE",
+        configuration = FeignConfig.class
+)
+public interface TransactionServiceClient {
+	@GetMapping("/api/transactions/internal/dashboard/total")
+	Long getTotalTransactions();
+
+	@GetMapping("/api/transactions/internal/dashboard/successful")
+	Long getSuccessfulTransactions();
+
+	@GetMapping("/api/transactions/internal/dashboard/failed")
+	Long getFailedTransactions();
+
+	@GetMapping("/api/transactions/internal/dashboard/amount")
+	BigDecimal getTotalTransactionAmount();
+
+}
