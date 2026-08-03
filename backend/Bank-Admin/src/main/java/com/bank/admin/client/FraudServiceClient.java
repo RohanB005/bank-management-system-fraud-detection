d166047253c90dfa5079811dc1d254e9f44ff2c8
Omkar-ijardar <omkarijardar@gmail.com>
2026-08-04 02:37:01 +0530

    implement Feign clients for microservice communications

---
+++ backend/Bank-Admin/src/main/java/com/bank/admin/client/FraudServiceClient.java
@@ -0,0 +1,19 @@
+package com.bank.admin.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bank.admin.config.FeignConfig;
+
+@FeignClient(
+        name = "FRAUD-SERVICE",
+        configuration = FeignConfig.class
+)
+public interface FraudServiceClient {
+
+	@GetMapping("/api/fraud/internal/dashboard/total")
+	Long getTotalFraudCases();
+
+	@GetMapping("/api/fraud/internal/dashboard/pending")
+	Long getPendingFraudCases();
+}
