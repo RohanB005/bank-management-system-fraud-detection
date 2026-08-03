6c8d4485546a4ca8841ec2486f1f4ec60a44a153
Omkar-ijardar <omkarijardar@gmail.com>
2026-08-04 02:37:15 +0530

    implement Feign clients for microservice communications

---
+++ backend/Bank-Admin/src/main/java/com/bank/admin/client/AuthServiceClient.java
@@ -0,0 +1,19 @@
+package com.bank.admin.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.bank.admin.config.FeignConfig;
+
+@FeignClient(
+        name = "AUTH-SERVICE",
+        configuration = FeignConfig.class
+)
+public interface AuthServiceClient {
+	@GetMapping("/api/auth/internal/dashboard/total-customers")
+	Long getTotalCustomers();
+
+	@GetMapping("/api/auth/internal/dashboard/active-customers")
+	Long getActiveCustomers();
+
+}
